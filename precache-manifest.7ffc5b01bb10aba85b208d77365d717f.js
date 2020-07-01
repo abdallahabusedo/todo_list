66f8924e780eddf37c03a65dcc15c45b9a82f459
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-07-01 17:26:41 +0200

    Deploy

---
+++ precache-manifest.7ffc5b01bb10aba85b208d77365d717f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5f28b9de60aab421e991c92e8f245f",
+    "url": "/todo_lists/index.html"
+  },
+  {
+    "revision": "612e6679899d162eb916",
+    "url": "/todo_lists/static/css/main.81c4d041.chunk.css"
+  },
+  {
+    "revision": "0ae9cd66f98f87eb79ee",
+    "url": "/todo_lists/static/js/2.ac1eaab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_lists/static/js/2.ac1eaab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612e6679899d162eb916",
+    "url": "/todo_lists/static/js/main.92808d13.chunk.js"
+  },
+  {
+    "revision": "2d68159978a2ff721b8a",
+    "url": "/todo_lists/static/js/runtime-main.c5143be1.js"
+  },
+  {
+    "revision": "1610a0ff82fcaf4f3ea5ba61b94dfc29",
+    "url": "/todo_lists/static/media/back.1610a0ff.jpg"
+  }
+]);
